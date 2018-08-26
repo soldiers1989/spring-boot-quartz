e5eb45d762d047776fb2b12db990698b1286cdcc
wangmeixin <mushishiginko@qq.com>
2018-08-26 19:38:25 +0800

    init

--- src/main/java/com/pgy/ginko/quartz/controller/test/GinkoController.java
+++ src/main/java/com/pgy/ginko/quartz/controller/test/GinkoController.java
@@ -1,5 +1,6 @@
 package com.pgy.ginko.quartz.controller.test;
 
+import com.pgy.ginko.quartz.service.biz.impl.HttpApiService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @author ginko
@@ -18,6 +21,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 @Api(tags = "1.0", description = "接口测试", value = "接口测试")
 public class GinkoController {
+
+    @Resource
+    private HttpApiService httpApiService;
+
+    @RequestMapping("httpclient")
+    @ApiOperation(value = "测试HttpApiService")
+    public String test() throws Exception {
+        String str = httpApiService.doGet("http://www.baidu.com");
+        System.out.println(str);
+        return "hello";
+    }
 
     @GetMapping("/{id}")
     @ApiOperation(value = "测试条件查询")
